773aa646e559db576fd1eeff0943ae78b4d177e5
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-13 14:15:29 -0600

    Insurance: Test agent

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,49 +13,6 @@ from insurance.services import (
 from insurance.repository.storage.memory import MemoryStorage
 
 
-def test_create_agent_with_number():
-    # initialize the storage
-    storage = MemoryStorage()
-
-    # create an agent with a number
-    agent_number = 123456789
-    agent_name = "John Doe"
-    storage, agent = create_agent(
-        storage,
-        agent_name,
-        agent_number,
-    )
-
-    assert isinstance(agent, Agent)
-    assert agent.name == agent_name
-    assert agent.number == agent_number
-    assert len(storage.get_agents()) == 1
-
-
-def test_create_agent_with_idempotency():
-    # initialize the storage
-    storage = MemoryStorage()
-
-    # create an agent with a number
-    agent_number = 123456789
-    agent_name = "John Doe"
-    storage, _ = create_agent(
-        storage,
-        agent_name,
-        agent_number,
-    )
-    storage, agent = create_agent(
-        storage,
-        agent_name,
-        agent_number,
-    )
-
-    assert isinstance(agent, Agent)
-    assert agent.name == agent_name
-    assert agent.number == agent_number
-    assert len(storage.get_agents()) == 1
-
-
 def test_create_policy_with_number():
     # initialize the storage
     storage = MemoryStorage()
